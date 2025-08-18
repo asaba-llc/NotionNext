db4a5e205aa535c3f98bd9fb31a6b17235d2c5ba
asaba-llc <ri@asaba.llc>
2025-08-18 14:36:40 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,6 +18,8 @@ const BLOG = {
   BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://asaba.llc', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '浅羽合同会社', // 网站关键词 英文逗号隔开
+  //SNS
+  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'ri@asaba.llc', // 邮箱地址 例如mail@tangly1024.com
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
